7e195d4b24b5e51b22c13f9cce0e1f91a43b6dca
Katharina von Sturm <katharinavonsturm@gmail.com>
2018-02-04 18:13:55 +0100

    debug

--- RA_PoiStat.h
+++ RA_PoiStat.h
@@ -63,7 +63,7 @@ class RA_PoiStat : public TObject
 
   // Katharina: Cumulative Brazilian Plot
   int cal_cumu( double obs, double nu, double s, double error, TString MCopt );
-  void Plot_Cumulative(TH1D* h_mc ,TH1D* h_data, Double_t Lumi_scale , Double_t Percent_error, Double_t Prob1, Double_t Prob2, Double_t Prob3, TString MCopt)
+  void Plot_Cumulative(TH1D* h_mc ,TH1D* h_data, Double_t Lumi_scale , Double_t Percent_error, Double_t Prob1, Double_t Prob2, Double_t Prob3, TString MCopt);
 
   // finding the Probability intervals
   int * Central_ProbSet(double nu, double prob, double s, double error, TString MCopt);
